08c1c0cf7e92df077a7950c0a6f29dc0cf4c91fc
lashmanbala <lashmanbala@gmail.com>
2024-05-09 07:36:24 +0530

    wrote a function to get column details of the datasets dynamically

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import glob
 import json
 
-src_file_names = glob.glob('data/retail_db/*/part-*', recursive=True)  # ** is to access all the files and folders
+src_file_names = glob.glob('data/retail_db/*/part-*', recursive=True) 
 #print(src_file_names)
 
 schema = json.load(open('data/retail_db/schemas.json'))
 #print(schema['departments'])
+clm = schema['orders']
+clms = sorted(clm, key=lambda col:col['column_position'])
+#for i in clms:
+#    print(i['column_name'])
+
+def get_column_details(schema,ds_name,sorting_key='column_position'):
+    column_details = schema[ds_name]
+    columns = sorted(column_details, key=lambda col:col[sorting_key])
+    return [col['column_name'] for col in columns]
+print(get_column_details(schema,'orders'))
+print(get_column_details(schema,'order_items'))
+print(get_column_details(schema,'departments'))
