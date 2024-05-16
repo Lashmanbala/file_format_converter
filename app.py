bb00587296eaac4fd85a2e93ae07a3b2e0fae15e
lashmanbala <lashmanbala@gmail.com>
2024-05-16 08:45:03 +0530

    reading all the csv file into pandas df dynamically

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import glob
 import json
+import pandas as pd
+import os
 
 src_file_names = glob.glob('data/retail_db/*/part-*', recursive=True) 
 #print(src_file_names)
@@ -15,6 +17,23 @@ def get_column_details(schema,ds_name,sorting_key='column_position'):
     column_details = schema[ds_name]
     columns = sorted(column_details, key=lambda col:col[sorting_key])
     return [col['column_name'] for col in columns]
-print(get_column_details(schema,'orders'))
-print(get_column_details(schema,'order_items'))
-print(get_column_details(schema,'departments'))
+
+#print(get_column_details(schema,'orders'))
+#print(get_column_details(schema,'order_items'))
+#print(get_column_details(schema,'departments'))
+
+#print(src_file_names[0].split('/')[2])
+#print(src_file_names[1].split('/')[2])
+#print(src_file_names[2].split('/')[2])
+
+#for file in src_file_names:
+#    print(file.split('/')[2])
+
+# reading all the files dynamically
+for file in src_file_names:
+    print(f'processing {file}')
+    ds_name = file.split('/')[2]
+    clms = get_column_details(schema, ds_name)
+    df = pd.read_csv(file, names=clms)
+    print(df.shape)
+
