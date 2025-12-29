679a5f63afc7eb9a67b6731b557e19c91469a9c3
lashmanbala <lashmanbala@gmail.com>
2025-12-29 13:07:46 +0530

    Implemented logging

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ def load_config():
     try:
         load_dotenv()
 
-        src_dir = os.environ.get("SRC_BASE_DIR ")
-        tgt_dir = os.environ.get("TGT_BASE_DIR ")
+        src_dir = os.environ.get("SRC_BASE_DIR")
+        tgt_dir = os.environ.get("TGT_BASE_DIR")
 
         if not src_dir:
             raise ValueError("SRC_BASE_DIR is not set in environment variables.")
@@ -18,14 +18,14 @@ def load_config():
         if not os.path.exists(src_dir):
             raise FileNotFoundError(f"Source directory '{src_dir}' does not exist.")
         
-        if not src_dir.os.path.isdir():
-            raise NotADirectoryError(f"Source path '{src_dir}' is not a directory.")
+        if not os.path.exists(tgt_dir):
+            os.makedirs(tgt_dir, exist_ok=True)
         
-        if not os.makedirs(tgt_dir, exist_ok=True):
-            raise OSError(f"Could not create target directory '{tgt_dir}': {e}")
-        
+        logger.info(f"Environment configuration loaded successfully.")
         return src_dir, tgt_dir
         
     except Exception as e:
         logger.warning(f"Error loading configuration: {e}")
         raise
+    
+load_config()
