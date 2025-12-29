0b794af764d78c6d473e941381fa73647d5969a7
lashmanbala <lashmanbala@gmail.com>
2025-12-29 12:57:30 +0530

    Updated

--- src/logger.py
+++ src/logger.py
@@ -7,22 +7,15 @@ def setup_logger():
         load_dotenv()
 
         log_file_path = os.environ.get('LOG_FILE_PATH')
-        log_file = os.environ.get('LOG_FILE')
 
         if not log_file_path:
             raise ValueError("log_file_path is not set in environment variables.")
-        
-        if not log_file:
-            raise ValueError("log_file is not set in environment variables.")
-
-        if not os.path.exists(log_file_path):
-            os.makedirs(log_file_path, exist_ok=True)
 
         logger = logging.getLogger(__name__)
 
         logger.setLevel(logging.INFO)
 
-        file_handler = logging.FileHandler(log_file, mode='a')
+        file_handler = logging.FileHandler(log_file_path, mode='a')
 
         file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 
