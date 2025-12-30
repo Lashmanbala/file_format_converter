ebdc4eb32e792b5376264357c11c25a1536be45c
lashmanbala <lashmanbala@gmail.com>
2025-12-30 07:31:59 +0530

    Getting column names of a dataset

--- src/schema.py
+++ src/schema.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from logger import logger
 
 def load_and_validate_schema(schema_path):
     if not schema_path.exists:
@@ -30,6 +31,24 @@ def load_and_validate_schema(schema_path):
                 missing = required_columns - col.keys()
                 raise ValueError(f"Columns {missing} are missing in column definition at index {i} in dataset {dataset_name}")
             
+    logger.info(f"Schema loaded and validated successfully from {schema_path}")
     return schema
 
-print(load_and_validate_schema(Path("data/retail_db/schemas.json")))
+
+def get_column_names(schema, dataset_name, sorting_key='column_position'):
+    if dataset_name not in schema:
+        raise ValueError(f"Dataset {dataset_name} not found in schema.")
+    
+    column_details = schema[dataset_name]
+
+    try:
+        sorted_columns = sorted(column_details, key=lambda col: col[sorting_key])
+    except KeyError:
+        raise ValueError(f"Sorting key '{sorting_key}' not found in column definitions for dataset {dataset_name}.")
+    
+    column_names = [col['column_name'] for col in sorted_columns]
+    
+    logger.info(f"Extracted {len(column_names)} columns for dataset {dataset_name} retrieved successfully.")
+    return column_names
+
+print(get_column_names(load_and_validate_schema(Path("data/retail_db/schemas.json")), "products"))
